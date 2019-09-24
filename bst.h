9b323cd872444ad4552462762f1bd2a621ebc154
yungene <32654121+yungene@users.noreply.github.com>
2019-09-24 19:04:57 +0100

    Implement contains() and isEmpty() functions.

--- bst.h
+++ bst.h
@@ -5,23 +5,24 @@
 // author: Jevgenijus Cistiakovas
 
 // A simple generic Binary Search Tree
-template<typename T>
-class BST
-{
-public:
+// TODO(j.cistiakovas): Add documentation.
+template <typename T>
+class BST {
+ public:
   BST() : root_(nullptr) {}
-	BST(TreeNode<T> node) : root_(node) {}
-	void insertNode(T val);
-	// returns nullptr if not found
-	TreeNode<T>* search(T val);
-	bool isEmpty();
-	bool contains(T val);
-	TreeNode<T>* lca(T val1, T val2);
-	// Use this for easier nesting such as lca(a,lca(b,c))
-	TreeNode<T>* lca(T val1, TreeNode<T>* val2_ptr);
-private:
-	TreeNode<T>* root_;
-	TreeNode<T>* searchUtil(TreeNode<T>* root, T val);
+  BST(TreeNode<T> node) : root_(node) {}
+  void insertNode(T val);
+  // returns nullptr if not found
+  TreeNode<T>* search(T val);
+  bool isEmpty();
+  bool contains(T val);
+  TreeNode<T>* lca(T val1, T val2);
+  // Use this for easier nesting such as lca(a,lca(b,c))
+  TreeNode<T>* lca(T val1, TreeNode<T>* val2_ptr);
+
+ private:
+  TreeNode<T>* root_;
+  TreeNode<T>* searchUtil(TreeNode<T>* root, T val);
   TreeNode<T>* insertUtil(TreeNode<T>* root, T val);
 };
 
@@ -33,7 +34,7 @@ void BST<T>::insertNode(T val) {
 template <typename T>
 TreeNode<T>* BST<T>::insertUtil(TreeNode<T>* root, T val) {
   if (!root) {
-    auto * new_node = new TreeNode<T>(val);
+    auto* new_node = new TreeNode<T>(val);
     return new_node;
   }
   T data = root->getData();
@@ -68,18 +69,18 @@ TreeNode<T>* BST<T>::searchUtil(TreeNode<T>* root, T val) {
   if (val < data) {
     return searchUtil(root->left_, val);
   } else {
-    return searchUtil(root->right_,val);
+    return searchUtil(root->right_, val);
   }
 }
 
 template <typename T>
 bool BST<T>::isEmpty() {
-  return false;
+  return !root_;
 }
 
 template <typename T>
 bool BST<T>::contains(T val) {
-  return search(val) != nullptr;
+  return search(val);
 }
 
 template <typename T>
